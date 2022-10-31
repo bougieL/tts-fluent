de74da184b6cc82826a7c85b449c8495aef57a8e
Bougie <bougie.liu@qq.com>
2022-10-31 10:39:56 +0800

    chore: update build script

--- scripts/build.js
+++ scripts/build.js
@@ -20,28 +20,26 @@ async function main(arg) {
     console.log(chalk.blueBright('Finish build transfer'));
   }
 
+  function buildPlatform(platform = 'all') {
+    console.log(chalk.blueBright(`Start build ${platform} platform`));
+    exec('rm -r release/build || true');
+    exec('rm -r release/app/dist || true');
+    buildTransfer();
+    exec('npm run build');
+    let args = '--win --mac --linux';
+    if (platform === 'win') {
+      args = '--win';
+    } else if (platform === 'mac') {
+      args = '--mac';
+    }
+    exec(`pnpm electron-builder build --publish never ${args}`);
+    console.log(chalk.blueBright(`Finish build ${platform} platform`));
+  }
+
   if (arg === 'transfer') {
     buildTransfer();
-  }
-
-  if (arg === 'all') {
-    console.log(chalk.blueBright('Start build all platform'));
-    exec('rm -r release/build || true');
-    exec('rm -r release/app/dist || true');
-    buildTransfer();
-    exec('npm run build');
-    exec('pnpm electron-builder build --publish never --win --mac --linux');
-    console.log(chalk.blueBright('Finish build all platform'));
-  }
-
-  if (!arg) {
-    console.log(chalk.blueBright('Start build current platform'));
-    exec('rm -r release/build || true');
-    exec('rm -r release/app/dist || true');
-    buildTransfer();
-    exec('npm run build');
-    exec('pnpm electron-builder build --publish never');
-    console.log(chalk.blueBright('Finish build current platform'));
+  } else {
+    buildPlatform(arg);
   }
 }
 
