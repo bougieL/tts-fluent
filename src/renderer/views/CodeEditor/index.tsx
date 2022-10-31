2da09a1d56b05647a00d684e3927127f9580cdc3
Bougie <bougie.liu@qq.com>
2022-10-31 14:31:33 +0800

    feat: init code mirror

--- src/renderer/views/CodeEditor/index.tsx
+++ src/renderer/views/CodeEditor/index.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react';
-import { UnControlled as CodeMirror } from 'react-codemirror2';
+import { useEffect, useRef, useState } from 'react';
+import { defaultKeymap } from '@codemirror/commands';
+import { javascript } from '@codemirror/lang-javascript';
+import { EditorState } from '@codemirror/state';
+import { EditorView, keymap } from '@codemirror/view';
 
 import { withWindow } from 'renderer/components';
 import { useFn } from 'renderer/hooks';
 
 import './index.scss';
-
-// import 'codemirror/mode/javascript/javascript';
 
 interface Props {
   initialData: {
@@ -17,21 +18,27 @@ interface Props {
 function CodeEditor({ initialData: { content } }: Props) {
   const [privContent, setPrivContent] = useState(content);
 
+  const divRef = useRef<HTMLDivElement>(null);
+
+  const editorRef = useRef<EditorView>();
+
   const handleChange = useFn((value: string) => {
     setPrivContent(value);
   });
 
-  return (
-    <CodeMirror
-      value="<h1>I ♥ react-codemirror2</h1>"
-      options={{
-        mode: 'javascript',
-        theme: 'material',
-        lineNumbers: true,
-      }}
-      onChange={(editor, data, value) => {}}
-    />
-  );
+  useEffect(() => {
+    const startState = EditorState.create({
+      doc: 'console.log("Hello World")',
+      extensions: [keymap.of(defaultKeymap), javascript()],
+    });
+
+    editorRef.current = new EditorView({
+      state: startState,
+      parent: divRef.current!,
+    });
+  }, []);
+
+  return <div ref={divRef} />;
 }
 
 export default withWindow(CodeEditor);
